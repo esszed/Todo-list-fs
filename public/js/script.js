5149facfe4db8e229560520d68cf2cf050df2b12
Štěpán Zoubek <stepan.zoubek@gmail.com>
2020-07-31 01:44:21 +0200

    DB implementation

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@ const links = document.querySelectorAll(".listLink");
 
 for (let i = 0; i < links.length; i++) {
   links[i].addEventListener("click", (e) => {
-    fetch(`/switch?name=${links[i].name}`).then((response) => {
+    fetch(`/switch?id=${links[i].name}`).then((response) => {
       if (response.ok) {
         window.location.href = "/";
         return;
@@ -15,7 +15,7 @@ for (let i = 0; i < links.length; i++) {
 const deleteLinks = document.querySelectorAll(".deleteListLink");
 for (let i = 0; i < links.length; i++) {
   deleteLinks[i].addEventListener("click", (e) => {
-    fetch(`/deletelist?name=${deleteLinks[i].name}`).then((response) => {
+    fetch(`/deletelist?id=${deleteLinks[i].name}`).then((response) => {
       if (response.ok) {
         window.location.href = "/";
         return;
--- src/app.js
+++ src/app.js
@@ -1,70 +1,109 @@
-const path = require("path");
-const express = require("express");
-const app = express();
-const ejs = require("ejs");
-const port = 3000;
+//modules
+const path = require('path')
+const express = require('express')
+const mongoose = require('mongoose')
+const ejs = require('ejs')
 
-const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates/views");
+const port = 3000
 
-app.set("view engine", "ejs");
-app.set("views", viewsPath);
+const app = express()
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(publicDirectoryPath));
+//paths
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
 
-let lists = [{ name: "Important", tasks: ["Buy milk"] }];
-let currentTask = 0;
-app.get("/", (req, res) => {
-  let today = new Date();
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-  let day = today.toLocaleDateString("en-UK", options);
-  let listName;
-  let tasks;
-  if (lists.length==0) {
-    listName = "";
-    tasks = [];
-  } else {
-    listName = lists[currentTask].name;
-    tasks = lists[currentTask].tasks;
+//view  engine
+app.set('view engine', 'ejs')
+app.set('views', viewsPath)
+
+//express settings
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(publicDirectoryPath))
+
+//database coonnect
+mongoose.connect('mongodb://localhost:27017/todolistDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
+
+const listsSchema = {
+  name: String,
+  items: []
+}
+const List = mongoose.model('List', listsSchema)
+
+const itemsSchema = {
+  name: String
+}
+
+const Item = mongoose.model('Item', itemsSchema)
+
+let currentNumber = 0
+app.get('/', (req, res) => {
+  let today = new Date()
+  const options = {
+    weekday: 'long',
+    day: 'numeric',
+    month: 'long'
   }
-  res.render("index", {
-    listTitle: day,
-    lists: lists,
-    name: listName,
-    tasks: tasks,
-  });
-});
+  let day = today.toLocaleDateString('en-UK', options)
 
-app.post("/addItem", (req, res) => {
-  lists[currentTask].tasks.push(req.body.newItem);
-  res.redirect("/");
-});
+  List.find({}, (err, lists) => {
+    let listName
+    let tasks
+    if (lists.length == 0) {
+      currentList = { name: '', items: [] }
+    } else {
+      currentList = lists[currentNumber]
+    }
+    res.render('index', {
+      day,
+      lists,
+      currentList
+    })
+  })
+})
 
-app.post("/addList", (req, res) => {
-  lists.push({ name: req.body.listName, tasks: [] });
-  res.redirect("/");
-});
+app.post('/addItem', (req, res) => {
+  lists[currentNumber].tasks.push(req.body.newItem)
+  res.redirect('/')
+})
 
-app.get("/switch", (req, res) => {
-  currentTask = lists.findIndex((element) => element.name == req.query.name);
-  res.redirect("/");
-});
+app.post('/addList', (req, res) => {
+  const list = new List({
+    name: req.body.listName,
+    tasks: []
+  })
+  list.save()
+  res.redirect('/')
+})
 
-app.get("/deletelist", (req, res) => {
-  let deleteList = lists.findIndex((element) => element.name == req.query.name);
-  if (deleteList == currentTask) {
-    currentTask = 0;
-  }
-  lists.splice(deleteList, 1);
-  res.redirect("/");
-});
+app.get('/switch', (req, res) => {
+  List.find({}, (err, lists) => {
+    currentNumber = lists.findIndex(element => element._id == req.query.id)
+    res.redirect('/')
+  })
+})
+
+app.get('/deletelist', (req, res) => {
+  List.find({}, (err, lists) => {
+    let index = lists.findIndex(element => element._id == req.query.id)
+    if (currentNumber === index) {
+      currentNumber = 0
+    }
+  })
+
+  List.findByIdAndRemove({ _id: req.query.id }, err => {
+    if (err) {
+      console.log(err)
+    }
+  })
+
+  res.redirect('/')
+})
 
 app.listen(port, () => {
-  console.log(`App is running on port:${port}`);
-});
+  console.log(`App is running on port:${port}`)
+})
